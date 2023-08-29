2909ec4fa4161248b63107e4d7c138a66627b6e5
Laman Ismayilova <laman.mamedova98@gmail.com>
2023-08-29 13:25:51 -0700

    updates1

--- server/server.js
+++ server/server.js
@@ -17,10 +17,16 @@ const {addSpendings,getSpendings,deleteSpendings,updateSpendings}=require('./con
 app.use(express.json())
 app.use(cors())
 app.use(express.static(path.resolve(__dirname,'../public')))
-// app.use(express.static(path.resolve(__dirname,'../img')))
+
 app.get('/',(req,res)=> {
   res.sendFile(path.resolve(__dirname,'../public/index.html'))
 })
+app.get('/plan',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'../public/planBudget.html'))
+  })
+  app.get('/previous',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'../public/previousBudgetList.html'))
+  })
 app.post('/api/seed',seed)
 app.post('/api/addSpendings',addSpendings)
 app.get('/api/getSpendings',getSpendings)
